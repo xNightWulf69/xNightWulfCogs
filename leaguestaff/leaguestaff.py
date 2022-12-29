40c8ab2ac31d95b7970621057edb4adb5e3c5ec4
xNightWulf69 <65742919+xNightWulf69@users.noreply.github.com>
2022-12-29 13:35:40 +0000

    Add files via upload

--- leaguestaff/leaguestaff.py
+++ leaguestaff/leaguestaff.py
@@ -19,6 +19,6 @@ class LeagueStaff(commands.Cog):
 
         embed = discord.Embed(title="The Swarm Staff", color=16772096, thumbnail_url="https://cdn.discordapp.com/attachments/1058010972138254348/1058012219390046288/IMG_0038.png")
         for member in members:
-            embed.add_field(name="<:swarm:1058011486116646942> Staff Member", value=member.mention, inline=True)
-        embed.set_footer(text=f"<:swarm:1058011486116646942> Total staff members: {len(members)}")
+            embed.add_field(name="<:swarm:1035174263147208805> Staff Member", value=member.mention, inline=True)
+        embed.set_footer(text=f"<:swarm:1035174263147208805> Total staff members: {len(members)}")
         await ctx.send(embed=embed)
