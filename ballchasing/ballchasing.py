e19d41788b0a0e961ed32aefda1d10bf7e9925e2
xNightWulf69 <65742919+xNightWulf69@users.noreply.github.com>
2022-12-29 16:21:08 +0000

    Add files via upload

--- ballchasing/ballchasing.py
+++ ballchasing/ballchasing.py
@@ -1,28 +1,20 @@
 import discord
 from redbot.core import commands
-import requests
 
 class Ballchasing(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
-    async def latestgame(self, ctx, *, player_name: str):
-        # Make a request to the Ballchasing API to get the latest game for the specified player
-        url = f"https://ballchasing.com/api/v1/player/{player_name}/latest"
-        response = requests.get(url)
-        if response.status_code != 200:
-            return await ctx.send("Failed to retrieve player stats. Please check the player name and try again.")
-        data = response.json()
-
-        # Extract the relevant information from the API response and format it for the Discord message
-        game_mode = data["game_mode"]
-        goals = data["stats"]["goals"]
-        assists = data["stats"]["assists"]
-        saves = data["stats"]["saves"]
-        shots = data["stats"]["shots"]
-        message = f"In their latest {game_mode} game, {player_name} scored {goals} goals, had {assists} assists, made {saves} saves, and took {shots} shots."
-
-        # Create an embed to send the stats in
-        embed = discord.Embed(title=f"{player_name}'s latest game stats", description=message, color=discord.Color.blue())
-        await ctx.send(embed=embed)
+    async def ballchasinglink(ctx, *, player_name: str):
+        # Replace spaces in the player name with %20
+        player_name = player_name.replace(" ", "%20")
+    
+        # If the player name is not specified, send a message asking for a player name
+        if not player_name:
+            await ctx.send("Please specify a player name")
+            return
+    
+        # Otherwise, send a message with the link to the Ballchasing website
+        link = f"https://ballchasing.com/?player-name={player_name}"
+        await ctx.send(link)
