13ed207e813948ae54b018edb8ba2eedffbd338f
xNightWulf69 <65742919+xNightWulf69@users.noreply.github.com>
2023-01-03 08:33:49 +0000

    Add files via upload

--- teammodule/teammodule.py
+++ teammodule/teammodule.py
@@ -6,7 +6,9 @@ team_config = Config.get_conf(None, identifier=1234567890, force_registration=Tr
 free_agents_config = Config.get_conf(None, identifier=12345678910, force_registration=True)
 # Define the team Config subgroup
 team_config.register_guild(
-    name='',
+    teams={}
+)
+team_config.register_team(
     general_manager=None,
     players=[]
 )
@@ -19,45 +21,49 @@ class TeamModule(commands.Cog):
 
     @commands.command()
     @commands.has_role(1025216358117544037)
-    async def create_team(self, ctx, general_manager: discord.Member, *, name: str):
-        """Creates a new team with the given name and the specified user as the general manager."""
-        # Check if the general manager is already on a team
-        if await team_config.guild(general_manager.guild).general_manager() is not None or general_manager.id in await team_config.guild(general_manager.guild).players():
-            await ctx.send(f'{general_manager.mention} is already on a team.')
+    async def create_team(self, ctx, general_manager: discord.Member, *, team_name: str):
+        # Retrieve the list of teams from the Config
+        teams = await self.team_config.guild(ctx.guild).teams()
+
+        # Check if the team already exists
+        if team_name in teams:
+            await ctx.send(f'A team with the name "{team_name}" already exists.')
             return
 
-        # Create the new team
-        await team_config.guild(ctx.guild).name.set(name)
-        await team_config.guild(ctx.guild).general_manager.set(general_manager.id)
-        await team_config.guild(ctx.guild).players.set([])
-        role = discord.utils.get(general_manager.guild.roles, id=1028690403022606377)
+        # Create a new entry in the Config for the team
+        teams[team_name] = {
+            'general_manager': general_manager.id,
+            'players': []
+        }
+        await self.team_config.guild(ctx.guild).teams.set(teams)
+
+        # Give the general manager the role
+        role = discord.utils.get(ctx.guild.roles, id=1028690403022606377)
         await general_manager.add_roles(role)
-        await ctx.send(f'{ctx.author.mention} has created the team "{name}" and has set {general_manager.mention} as the general manager.')
+
+        await ctx.send(f'Team "{team_name}" has been created with {general_manager.mention} as the general manager.')
 
     @commands.command()
     async def leave_team(self, ctx):
-        """Leaves the current team."""
-        # Check if the user is the general manager
-        if ctx.author.id == await self.team_config.guild(ctx.guild).general_manager():
-            # Remove the general manager role from the general manager
-            role = discord.utils.get(ctx.author.guild.roles, id=1028690403022606377)
-            await ctx.author.remove_roles(role)
+        # Retrieve the list of teams from the Config
+        teams = await self.team_config.guild(ctx.guild).teams()
 
-            # Clear the team's general manager
-            await self.team_config.guild(ctx.guild).general_manager.set(None)
+        # Find the team that the user is on
+        for team_name, team_info in teams.items():
+            if ctx.author.id in team_info['players'] or ctx.author.id == team_info['general_manager']:
+                # Remove the user from the team
+                if ctx.author.id == team_info['general_manager']:
+                    team_info['general_manager'] = None
+                else:
+                    team_info['players'].remove(ctx.author.id)
 
-            await ctx.send(f'{ctx.author.mention} has left the team.')
+                # Update the Config
+                await self.team_config.guild(ctx.guild).teams.set(teams)
+
+                await ctx.send(f'{ctx.author.mention} has left the team {team_name}.')
+                return
         else:
-            # Check if the user is a player
-            players = await self.team_config.guild(ctx.guild).players()
-            if ctx.author.id in players:
-                # Remove the player from the player list
-                players.remove(ctx.author.id)
-                await self.team_config.guild(ctx.guild).players.set(players)
-
-                await ctx.send(f'{ctx.author.mention} has left the team.')
-            else:
-                await ctx.send(f'{ctx.author.mention} is not on a team.')
+            await ctx.send(f'{ctx.author.mention} is not on a team.')
 
     @commands.command()
     async def register(self, ctx, mmr: int, tracker_link: str):
