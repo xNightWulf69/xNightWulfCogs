7867c53f17dd5f1eee13f76cad1213831c04845b
xNightWulf69 <65742919+xNightWulf69@users.noreply.github.com>
2023-01-03 20:08:56 +0000

    Add files via upload

--- teammodule/teammodule.py
+++ teammodule/teammodule.py
@@ -42,7 +42,7 @@ class TeamModule(commands.Cog):
             embed.add_field(name="General Manager", value=gmid.mention, inline=False)
             for player in players:
                 playerid = self.bot.get_user(int(player))
-                test = str(teams[team_name]["players"][player]["mmr"]) / 100
+                test = int(teams[team_name]["players"][player]["mmr"]) / 100
                 embed.add_field(name="Player", value=playerid.mention + " " + test, inline=False)
                 await ctx.send(embed=embed)
         else:
