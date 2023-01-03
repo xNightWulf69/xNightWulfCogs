1226b650f10cf89725dd16456514131b29b8cf32
xNightWulf69 <65742919+xNightWulf69@users.noreply.github.com>
2023-01-03 07:48:18 +0000

    Add files via upload

--- teammodule/teammodule.py
+++ teammodule/teammodule.py
@@ -3,18 +3,19 @@ from redbot.core import Config, commands
 
 # Create a new Config instance for storing team information
 team_config = Config.get_conf(None, identifier=1234567890, force_registration=True)
-
+free_agents_config = Config.get_conf(None, identifier=1234567890, force_registration=True)
 # Define the team Config subgroup
 team_config.register_guild(
     name='',
     general_manager=None,
     players=[]
 )
-
+free_agents_config.register_guild(**{str(i): {'mmr': 0, 'tracker_link': ''} for i in range(100)})
 class TeamModule(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.team_config = team_config
+        self.free_agents_config = free_agents_config
 
     @commands.command()
     @commands.has_role(1025216358117544037)
@@ -58,8 +59,18 @@ class TeamModule(commands.Cog):
             else:
                 await ctx.send(f'{ctx.author.mention} is not on a team.')
 
-class Team:
-    def __init__(self, name, general_manager, players=[]):
-        self.name = name
-        self.general_manager = general_manager
-        self.players = players
+    @commands.command()
+    async def register(self, ctx, mmr: int, tracker_link: str):
+        """Registers the user as a free agent with the given MMR and tracker link."""
+        # Check if the user is already on a team
+        if await self.team_config.guild(ctx.guild).general_manager() == ctx.author.id or ctx.author.id in await self.team_config.guild(ctx.guild).players():
+            await ctx.send(f'{ctx.author.mention} is already on a team.')
+            return
+
+        # Register the user as a free agent
+        await self.free_agents_config.guild(ctx.guild).set_raw(str(ctx.author.id), value={'mmr': mmr, 'tracker_link': tracker_link})
+
+        # Send a message in the channel and send the user's name, MMR, and tracker link to the designated channel
+        await ctx.send(f'{ctx.author.mention} has registered as a free agent with MMR {mmr} and tracker link {tracker_link}.')
+        channel = self.bot.get_channel(1059726875527762012)
+        await channel.send(f'{ctx.author.mention} (MMR: {mmr}, Tracker: {tracker_link})')
