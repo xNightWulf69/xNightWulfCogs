26f62c10f1d458b598762969ea3a8db8e9f76692
xNightWulf69 <65742919+xNightWulf69@users.noreply.github.com>
2023-01-05 19:14:44 +0000

    Add files via upload

--- teammodule/teammodule.py
+++ teammodule/teammodule.py
@@ -380,11 +380,11 @@ class TeamModule(commands.Cog):
             if f"{player.id}" in team["subplayers"]:
                 # Create an embed with the player's information
                 embed = discord.Embed(title=player.display_name, color=discord.Color.blue())
-                embed.add_field(name="Tracker", value=team["players"][f"{player.id}"]["tracker"], inline=False)
-                embed.add_field(name="MMR", value=team["players"][f"{player.id}"]["mmr"], inline=False)
+                embed.add_field(name="Tracker", value=team["players"][player.id]["tracker"], inline=False)
+                embed.add_field(name="MMR", value=team["players"][player.id]["mmr"], inline=False)
                 embed.add_field(name="Team", value=t_name, inline=False)
                 embed.add_field(name="Position", value="Sub", inline=False)
-                embed.add_field(name="In-game Name", value=team["players"][f"{player.id}"]["IGN"], inline=False)
+                embed.add_field(name="In-game Name", value=team["players"][player.id]["IGN"], inline=False)
 
                 await ctx.send(embed=embed)
                 return
